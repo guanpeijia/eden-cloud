e4f8fe77382c0a20f56fc5a88d04ef28c3e3a20a
guanpeijia <guanpeijia@qq.com>
2020-07-16 21:24:27 +0800

    eden-cloud init

---
+++ eden-server/eden-server-starter/src/main/java/com/gpj/EdenServerStarterApplication.java
@@ -0,0 +1,13 @@
+package com.gpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdenServerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdenServerStarterApplication.class, args);
+    }
+
+}
---
+++ eden-server/eden-server-starter/src/test/java/com/gpj/EdenServerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdenServerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
