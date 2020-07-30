27c32fb575cbc15d2810052d41edcc7d4a045443
guanpeijia <guanpeijia@qq.com>
2020-07-30 20:18:58 +0800

    搭建微服务注册中心

---
+++ eden-register/src/test/java/com/gpj/EdenRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdenRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
