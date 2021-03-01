0a50112432876b8a253f93285bedfe1c8f43a672
guanpeijia <guanpeijia@vip.qq.com>
2021-03-01 14:48:25 +0800

    整合Lettuce

--- eden-auth/src/main/java/com/EdenAuthApplication.java
+++ eden-auth/src/main/java/com/EdenAuthApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import com.peijia.common.annotation.EnableEdenAuthExceptionHandler;
 import com.peijia.common.annotation.EnableEdenServerProtect;
+import com.peijia.common.redis.EnableLettuceRedis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableEdenServerProtect
 @EnableDiscoveryClient
 @EnableEdenAuthExceptionHandler
+@EnableLettuceRedis
 @SpringBootApplication
 @MapperScan("com.peijia.auth.*.mapper")
 public class EdenAuthApplication {
